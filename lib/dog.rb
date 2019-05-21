64e6354a417229a436747c33e7bed4061079c947
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-05-21 15:15:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog 
+  
+  def name=(dogs_name) #sets var 
+    @dogs_name = dogs_name
+  end
+  
+  def name #getter returns var 
+    @dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end  
+  
