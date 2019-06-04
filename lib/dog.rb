af27bd7be68d52aac4a383417ca42f56a22217a5
James Britton <brittonjp@gmail.com>
2019-06-04 18:36:24 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,15 @@
-class Dog
-  def name=(dog_name)
-    @this_dogs_name = dog_name
+class Dog 
+  
+  def name=(name)
+    @name = name
   end
   
   def name
-    @this_dogs_name
+    @name
   end
   
   def bark
     puts "woof!"
   end
+  
 end
